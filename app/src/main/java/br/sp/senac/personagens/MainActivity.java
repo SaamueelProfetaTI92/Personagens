1fea765fe40ad766582011fb3abec0f4c0126679
samuel <saamuka70@gmail.com>
2019-08-11 21:08:37 -0300

    Hello

--- app/src/main/java/br/sp/senac/personagens/MainActivity.java
+++ app/src/main/java/br/sp/senac/personagens/MainActivity.java
@@ -2,13 +2,56 @@ package br.sp.senac.personagens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtUsuario, txtSenha;
+    Button btnEntrar, btnSair;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtUsuario = (EditText) findViewById(R.id.txtUsuario);
+        txtSenha = (EditText) findViewById(R.id.txtSenha);
+
+        txtUsuario.requestFocus();
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario, senha;
+                usuario = txtUsuario.getText().toString();
+                senha = txtSenha.getText().toString();
+
+                if (usuario.equals("Samuel") && senha.equals("samuel"))
+                {
+                    Intent  intent = new Intent(getApplicationContext(), Recebe_foto_nome.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Toast.makeText(getApplicationContext(), "Usuário e Senha Invalidos", Toast.LENGTH_SHORT).show();
+                    txtUsuario.setText("");
+                    txtSenha.setText("");
+                    txtUsuario.requestFocus();
+
+                }
+            }
+        });
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/br/sp/senac/personagens/Recebe_foto_nome.java
@@ -0,0 +1,14 @@
+package br.sp.senac.personagens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recebe_foto_nome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recebe_foto_nome);
+    }
+}
